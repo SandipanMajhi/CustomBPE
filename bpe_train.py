8c940e0a731cb7a3874ea06b3521017c36cb304b
SandipanMajhi <sandipan.majhi.email@gmail.com>
2025-03-29 23:14:41 +0530

    BPE Trainer

--- bpe_train.py
+++ bpe_train.py
@@ -9,9 +9,11 @@ from Model.Tokenizer import AutoTokenizer
 if __name__ == "__main__":
     data = pd.read_csv("Data/BookCorpus3.csv")
     bpe_model = BPEModel(is_train = True)
+    batch_size = 1000
 
-    for i in tqdm(range(data.shape[0])):
-        text = data.iloc[i]["0"]
+    for i in tqdm(range(0, data.shape[0], batch_size)):
+        text = [data.iloc[j,0] for j in range(i, i+batch_size)]
+        text = " ".join(text)
         bpe_model.train(text, special_tokens = ["[CLS]", "[SEP]", "[MASK]", "[PAD]"])
 
         bpe_model.load_inverse_vocab()
